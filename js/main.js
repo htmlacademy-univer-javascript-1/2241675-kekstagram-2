fa022e8565b90187e3a7b689a59e539eb8e95fe1
Tatiana <tashkevich90@mail.ru>
2023-03-15 23:09:28 +0500

    main.js

---
+++ js/main.js
@@ -0,0 +1,32 @@
+function getRandomInt(from, before) {
+if (from < 0 || before < 0)
+throw new RangeError('числа в диапазоне должны быть положительными')
+}
+
+if (typeof from === 'string' || typeof before === 'string') {
+throw new RangeError ('Значение должно быть числовым, не строкой');
+}
+  if (from === before) {
+    return from;
+  }
+if (from > before) {
+  [from, before]=[before, from]
+}
+
+{
+  from= Math.ceil(from);
+  before = Math.floor(before);
+  return Math.floor(Math.random() * ( before - from + 1)) + from; //Максимум и минимум включаются
+}
+
+function isCorrectLenght (str, maxLenght) {
+  if (typeof str !== 'string') {
+    throw new RangeError('Значение str должно быть строкой');
+  }
+
+  return (str.lenght <= maxLenght);
+}
+
+  export {getRandomInt,RandomInteger}
+
+  /* Используемый источник:https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random*/
